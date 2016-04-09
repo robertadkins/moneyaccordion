be0d8b937ed4b400d3601c34bf4d3c088402db6e
Steven Bower <sbower@umd.edu>
2016-04-09 11:21:19 -0400

    camera diff stuff
    potential for motion tracking/object detection to follow

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import moneydetector as md
+import time
 
 #HEY HEY Maybe we should look into tracking fingers holding the bill rather than the bill itself
 #easier to measure velocity, can't really measure bend in the dollar
@@ -13,10 +14,24 @@ cap = cv2.VideoCapture(0)
 
 #while(False):
 while(True):
-    ret, frame = cap.read() #read a frame
-    cv2.imshow('frame', frame) #display it
+    ret, frame1 = cap.read() #read a frame
+    #cv2.imshow('frame', frame) #display it
 
-    md.detect(frame)
+    #if you're checking for contours
+    #md.detect(frame)
+
+    time.sleep(.01)
+    ret, frame2 = cap.read()
+
+    diff = cv2.subtract(frame2, frame1)
+
+    #for later
+    #diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    #diff = cv2.adaptiveThreshold(diff, 1, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 0)
+
+    res = cv2.bitwise_and(frame2, diff)
+
+    cv2.imshow("frame", diff)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
