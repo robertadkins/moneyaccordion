2e1fccbfffdbeebd88c2d51c6a0a06ab0a9ec566
Jonah Chazan <person.1123.loztp@gmail.com>
2016-04-09 14:29:03 -0400

    Added blurred version of difference frame

--- camera.py
+++ camera.py
@@ -26,3 +26,26 @@ def diff_frames(frame):
     prev_frame = frame
     
     return res
+
+def diff_frames_blurred(frame):
+    global prev_frame
+    
+    if prev_frame is None:
+        prev_frame = frame
+
+    diff = cv2.subtract(frame, prev_frame)
+
+    #for later
+    diff_mod = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4,4))
+    cv2.filter2D(diff_mod, -1, disc, diff_mod)
+
+    ret, thresh = cv2.threshold(diff_mod, 100, 255, 0)
+    thresh = cv2.merge((thresh,thresh, thresh))
+    diff_mod = cv2.cvtColor(diff_mod, cv2.COLOR_GRAY2RGB)
+
+    res = cv2.bitwise_and(frame, diff_mod)
+    
+    prev_frame = frame
+    
+    return res
