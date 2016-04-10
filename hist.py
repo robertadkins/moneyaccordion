ef99b05e52a1b48819242849633e3b6db79ceeff
Robert Adkins <robertadkins00@gmail.com>
2016-04-10 06:07:41 -0400

    integrated dollar thresholding

--- hist.py
+++ hist.py
@@ -75,26 +75,15 @@ while(True):
         frame2 = hist_utils.draw_rects(frameorig, horizontal=False)
         cv2.imshow("frame", frame2)
 
-    elif not trained_dollar:
-        start_timer += 1
-        if cv2.waitKey(1) & 0xFF == ord('p'):# or start_timer == 100:
-            dollar_hist = hist_utils.get_hist(frameorig)
-            trained_dollar = True
+    #elif not trained_dollar:
+    #   start_timer += 1
+    #   if cv2.waitKey(1) & 0xFF == ord('p'):# or start_timer == 100:
+    #        dollar_hist = hist_utils.get_hist(frameorig)
+    #        trained_dollar = True
 
-        frame2 = hist_utils.draw_rects(frameorig)
-        cv2.imshow("frame", frame2)
+    #    frame2 = hist_utils.draw_rects(frameorig)
+    #    cv2.imshow("frame", frame2)
     else:
-        hist = hist_utils.hist_filter(frameorig, dollar_hist)
-        grayhist = cv2.cvtColor(hist, cv2.COLOR_BGR2GRAY)
-
-        combined = cv2.bitwise_and(edge, grayhist)
-        cv2.imshow("edge", edge)
-
-        diffframe1 = camera.diff_frames_blurred(frame1)
-        frame1 = hist_utils.hist_filter(diffframe1, dollar_hist)
-
-        # calculate average frame over maxFrames
-
         if len(frames) < maxFrames:
             frames.append(frame1)
         else:
@@ -108,86 +97,65 @@ while(True):
         summedFrame /= len(frames)
         summedFrame = summedFrame.astype("uint8")
 
-        frame2 = cv2.cvtColor(summedFrame, cv2.COLOR_BGR2GRAY)
+        ### PUT THRESHOLD HERE
+        gray = cv2.cvtColor(summedFrame, cv2.COLOR_BGR2GRAY)
+	
+	greenLower = (30, 10, 0)
+	greenUpper = (140, 200, 255)
+	
+	hsv = cv2.cvtColor(summedFrame, cv2.COLOR_BGR2HSV)
+	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	mask = cv2.bitwise_and(gray, mask)
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,\
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+	if len(cnts) > 0:
+            maxcnt = None
+            maxarea = 0
+            for cnt in cnts:
+                newarea = cv2.contourArea(cnt)
+                if newarea > maxarea:
+                    maxarea = newarea
+                    maxcnt = cnt
+            hull = cv2.convexHull(maxcnt)
+            cv2.drawContours(frameorig, [hull], 0, (0,0,255), 3)
 
-        ret, framet = cv2.threshold(frame2, 100, 255, 0)
-        framet = cv2.morphologyEx(framet, cv2.MORPH_CLOSE, np.ones((2,2),np.uint8), iterations=10)
-        framet1 = np.copy(framet)
-        #_,contours,hierarchy = cv2.findContours(framet, 1, 2)
-        contours, hierarchy = cv2.findContours(framet, 1, 2)
-
-
-        framet = cv2.cvtColor(framet, cv2.COLOR_GRAY2RGB)
-        allpts = []
-        if len(contours) > 0:
-            #print contours
-            print len(contours)
-
+            M = cv2.moments(maxcnt)
             mean = [0,0]
-            # assumption: each contour is tiny
-            for cnt in contours:
-                # print cnt
-                mean[0] += cnt[0][0][0]
-                mean[1] += cnt[0][0][1]
-            mean[0] /= len(contours)
-            mean[1] /= len(contours)
-
-            stddev = 0
-            for cnt in contours:
-                stddev += (cnt[0][0][0] - mean[0]) * (cnt[0][0][0] - mean[0])
-                stddev += (cnt[0][0][1] - mean[1]) * (cnt[0][0][1] - mean[1])
-            stddev /= len(contours)
-            stddev = math.sqrt(stddev)
-
-            for cnt in contours:
-                #print cnt.tolist()
-                if math.sqrt((cnt[0][0][0] - mean[0]) * (cnt[0][0][0] - mean[0]) + (cnt[0][0][1] - mean[1]) * (cnt[0][0][1] - mean[1])) < stddev:
-                    allpts = allpts + cnt.tolist()
-                    cv2.drawContours(summedFrame, cnt, -1, (255,0,0), 3)
-
-            #print allpts
-            if len(allpts) > 0:
-                hull = cv2.convexHull(np.array(allpts))
-                cv2.drawContours(summedFrame, [hull], -1, (0,255,0), 3)
+            mean[0] = int(M['m10']/M['m00'])
+            mean[1] = int(M['m01']/M['m00'])
                 
-                #M = cv2.moments(hull)
-                #cx = int(M['m10']/M['m00'])
-                #cy = int(M['m01']/M['m00'])
-
-                left = frameorig[:,0:mean[0]]
-                dist_left = 0
-                if len(left) != 0:
-                    for i in range(len(left)):
-                        summedFrame[i, mean[0], 0] = 255
-                    #dist_left, farthest_point_left, hand_isolated_frame_left = hist_utils.find_hand_farthest_point(left, hand_hist, mean)
-                    farthest_point_left = hist_utils.get_color_point(left, hand_hist)
+            left = frameorig[:,0:mean[0]]
+            dist_left = 0
+            if len(left) != 0:
+                for i in range(len(left)):
+                    summedFrame[i, mean[0], 0] = 255
+                farthest_point_left = hist_utils.get_color_point(left, hand_hist)
                   
-                       
-                right = frameorig[:,mean[0]:]
-                dist_right = 0
-                if len(right) != 0:
-                    # dist_right, farthest_point_right, hand_isolated_frame_right = hist_utils.find_hand_farthest_point(right, hand_hist, mean)
-                    farthest_point_right = hist_utils.get_color_point(right, hand_hist)
+            right = frameorig[:,mean[0]:]
+            dist_right = 0
+            if len(right) != 0:
+                farthest_point_right = hist_utils.get_color_point(right, hand_hist)
 
-                cv2.circle(frameorig, (mean[0], mean[1]), 5, [255,0,255], -1)
+            cv2.circle(frameorig, (mean[0], mean[1]), 5, [255,0,255], -1)
+            
+            left_open = True if farthest_point_left is None else farthest_point_left[1] < mean[1] + pointing_threshold
+            right_open = True if farthest_point_right is None else farthest_point_right[1] < mean[1] + pointing_threshold
 
-                
-                           # syn.modSynth(hull, dist_left < pointing_threshold, dist_right < pointing_threshold)
-                left_open = True if farthest_point_left is None else farthest_point_left[1] < mean[1] + pointing_threshold
-                right_open = True if farthest_point_right is None else farthest_point_right[1] < mean[1] + pointing_threshold
-
-                if farthest_point_right is not None:
-                    farthest_point_right = (farthest_point_right[0] + mean[0], farthest_point_right[1])
-                    cl = [0,255,0] if not right_open else [0,255,255]
-                    cv2.circle(frameorig, (int(farthest_point_right[0]),int(farthest_point_right[1])), 5, cl, -1)
-                if farthest_point_left is not None:
-                    cl = [0,255,0] if not left_open else [0,0,255]
-                    cv2.circle(frameorig, (int(farthest_point_left[0]),int(farthest_point_left[1])), 5, cl, -1)
-                    print "left open: ",left_open
-                    print "left: ", farthest_point_left[1]
-                    print "mean: ", mean[1]
-                    print "th: ", mean[1] - pointing_threshold
-                syn.modSynth(hull, left_open, right_open)
+            if farthest_point_right is not None:
+                farthest_point_right = (farthest_point_right[0] + mean[0], farthest_point_right[1])
+                cl = [0,255,0] if not right_open else [0,255,255]
+                cv2.circle(frameorig, (int(farthest_point_right[0]),int(farthest_point_right[1])), 5, cl, -1)
+            if farthest_point_left is not None:
+                cl = [0,255,0] if not left_open else [0,0,255]
+                cv2.circle(frameorig, (int(farthest_point_left[0]),int(farthest_point_left[1])), 5, cl, -1)
+                print "left open: ",left_open
+                print "left: ", farthest_point_left[1]
+                print "mean: ", mean[1]
+                print "th: ", mean[1] - pointing_threshold
+            syn.modSynth(hull, left_open, right_open)
                            
         addKeyboard(frameorig)
         cv2.imshow("frame", frameorig)
